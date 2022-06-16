b2c67b2db889ed1b6726ae76e7c2923793d945a8
Tobian <alejandrotobian@gmail.com>
2022-06-16 09:33:04 -0500

    Repositorio JavaFX, 16/06/22

---
+++ src/aplicacionventas/AplicacionVentas.java
@@ -0,0 +1,34 @@
+package aplicacionventas;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Tobian
+ */
+public class AplicacionVentas extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+     @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/VentanaPrincipal.fxml"));        
+        Scene scene = new Scene(root);
+        Image image =  new Image("/Img/icono-tiempo.png");
+        stage.getIcons().add(image);
+        stage.setTitle("PROYECTO PORTAFOLIO");
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+}
